b8d6fb7e8159eb643476f89f46c72df3dafe75f8
TaQuangKhoi <khoi0941@gmail.com>
2023-03-02 20:51:23 +0700

    Tạo View AccountLOLInfo

---
+++ Controllers/AccountLOLInfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CayThue.Controllers;
+
+public class AccountLOLInfoController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=webcaythue}/{id?}");
+    pattern: "{controller=AccountLOLInfo}/{action=Index}/{id?}");
 
 app.Run();
