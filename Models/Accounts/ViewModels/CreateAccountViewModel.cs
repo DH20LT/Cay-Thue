26e197b105fa14b606d276c54e88c3ead7395cec
TaQuangKhoi <khoi0941@gmail.com>
2023-03-11 01:45:01 +0700

    Tạo AccountCreate.cshtml và CreateAccountViewModel.cs

---
+++ Models/Accounts/ViewModels/CreateAccountViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CayThue.Models.Accounts.ViewModels;
+
+public class CreateAccountViewModel
+{
+    public Guid Id { get; set; }
+    
+    public Guid? GameId { get; set; }
+    
+    public float? Price { get; set; }
+    
+    public string? Description { get; set; }
+    
+    public string? UserId { get; set; }
+    
+    public bool? IsVerify { get; set; }
+    
+    public bool? CanChangeInfo { get; set; }
+    
+    public bool? IsSold { get; set; }
+    
+    public DateTime? CreatedDate { get; set; }
+}
