22f174acc9bf0a04935d21b8598fe7439c4d29f8
TaQuangKhoi <khoi0941@gmail.com>
2023-03-11 00:27:02 +0700

    Create TradeController.cs and rename ViewGiaoDich.cshtml

---
+++ Controllers/TradeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CayThue.Controllers;
+
+public class TradeController : Controller
+{
+    // GET
+    public IActionResult ViewGiaoDich()
+    {
+        return View();
+    }
+}
