8f5e4418511206ce9acb04e916d37452ef0abdc2
Nguyễn Hoàng Tâm <nht30112002@gmail.com>
2023-03-12 14:59:05 +0700

    tao view moi va add du lieu cho view ListAccount

---
+++ Controllers/ExhibitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CayThue.Controllers
+{
+    public class ExhibitController : Controller
+    {
+        public IActionResult Exhibit()
+        {
+            return View();
+        }
+    }
+}
