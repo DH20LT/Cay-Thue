fd859f69e3d2d433c5f58542c3b3ff515192feaf
TaQuangKhoi <khoi0941@gmail.com>
2023-03-13 11:13:22 +0700

    Viết 2 hàm toString cho AccountLolInfoRep.cs và Account.cs

--- Models/Accounts/Account.cs
+++ Models/Accounts/Account.cs
@@ -9,22 +9,26 @@ public class Account
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("ID")]
     public Guid Id { get; set; }
-    
-    [Column("GameID")]
-    public Guid? GameId { get; set; }
-    
+
+    [Column("GameID")] public Guid? GameId { get; set; }
+
     public float? Price { get; set; }
-    
+
     public string? Description { get; set; }
-    
+
     public string? UserId { get; set; }
-    
+
     public bool? IsVerify { get; set; }
-    
+
     public bool? CanChangeInfo { get; set; }
-    
+
     public bool? IsSold { get; set; }
-    
+
     public DateTime? CreatedDate { get; set; }
-    
+
+    public string? toString()
+    {
+        return "Account: " + Id + " " + GameId + " " + Price + " " + Description + " " + UserId + " " + IsVerify + " " +
+               CanChangeInfo + " " + IsSold + " " + CreatedDate;
+    }
 }
